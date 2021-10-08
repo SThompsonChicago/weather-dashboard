d85acdc0e8ff96073f7ccd3b6469e55beac07567
Stephen Thompson <s31@umbc.edu>
2021-10-08 13:57:23 -0500

    created js and css files

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var tempEl = document.getElementById('temp');
+
+var requestUrl ='https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=ad54bebac58cc390e5e8bdbe0c2225af';
+
