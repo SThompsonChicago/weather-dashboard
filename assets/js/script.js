48d5793ee6f9abc60bc86d9a72959a52046724bd
Stephen Thompson <s31@umbc.edu>
2021-10-11 21:08:52 -0500

    display current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-var tempEl = document.getElementById('temp');
+var tempEl;
 
-var requestUrl ='https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=ad54bebac58cc390e5e8bdbe0c2225af';
+$("#temp").html("Hello");
 
+function getApi() {
+    var requestUrl ='https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily&appid=ad54bebac58cc390e5e8bdbe0c2225af';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        $('#temp').html("Temp: " + data.current.temp +"&#8457");
+        $('#wind').html("Wind: " + data.current.wind_speed + " MPH");
+        $('#hum').html("Humidity: " + data.current.humidity + "%");
+        $('#uvInd').html("UV Index: " + data.current.uvi);
+      });
+  }
+  
+  getApi();
+
+  
