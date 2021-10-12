002fdd78dbf239860484925a587d64e6f16b4917
Stephen Thompson <s31@umbc.edu>
2021-10-12 01:55:09 -0500

    changed code to give current weather based on city name instead of coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
-var tempEl;
+var APIKey = "ad54bebac58cc390e5e8bdbe0c2225af";
+var city = "Baltimore";
 
-$("#temp").html("Hello");
+
 
 function getApi() {
-    var requestUrl ='https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily&appid=ad54bebac58cc390e5e8bdbe0c2225af';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        $('#temp').html("Temp: " + data.current.temp +"&#8457");
-        $('#wind').html("Wind: " + data.current.wind_speed + " MPH");
-        $('#hum').html("Humidity: " + data.current.humidity + "%");
-        $('#uvInd').html("UV Index: " + data.current.uvi);
-      });
-  }
-  
-  getApi();
+  var requestUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      $('#temp').html("Temp: " + data.main.temp +"&#8457");
+      $('#wind').html("Wind: " + data.wind.speed + " MPH");
+      $('#hum').html("Humidity: " + data.main.humidity + "%");
+    });
+}
 
+getApi();
   
