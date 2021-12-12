7330494ad6a53912c48f1e9ca9bc99b97950d78e
Stephen Thompson <s31@umbc.edu>
2021-12-12 02:20:24 -0600

    show icon for current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var APIKey = "ad54bebac58cc390e5e8bdbe0c2225af";
+
 var cityFormEl = document.querySelector('#city-form');
 var cityInputEl = document.querySelector('#user-city');
+var pic = document.querySelector('#pic');
+var weatherIcon = document.createElement('img');
 
 
 var formSubmitHandler = function(event) {
@@ -23,11 +26,17 @@ var getApi = function(city) {
       $('#temp').html("Temp: " + data.main.temp +"&#8457");
       $('#wind').html("Wind: " + data.wind.speed + " MPH");
       $('#hum').html("Humidity: " + data.main.humidity + "%");
+      var iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      
+      weatherIcon.setAttribute('src', iconUrl);
+      weatherIcon.setAttribute('class', 'weather-img');
+      pic.append(weatherIcon);
     });
+
+
+
+
 }
-
-
-
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
 
@@ -35,9 +44,3 @@ cityFormEl.addEventListener('submit', formSubmitHandler);
 /* $('h1#currentWeather').html("Current weather in " + cityInputEl.value); */
 
 
-
-
-
-
-getApi();
-  
